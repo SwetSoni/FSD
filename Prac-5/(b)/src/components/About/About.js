5c95992fb30daa170b2991e66cad194928aac7b9
SJM <swet2000sj.ss@gmail.com>
2022-08-28 22:49:19 +0530

    Update About.js

--- Prac-5/(b)/src/components/About/About.js
+++ Prac-5/(b)/src/components/About/About.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import './About.css';
+import React from "react";
+import "./About.css";
 
-export default function About(){
-    return(
-        <div className="About">
-            <div className="About-container">
-                <div className="About-header">
-                    <h1>About</h1>
-                    <h1>D21DCS149 Yash Chikhaliya</h1>
-                    <p>
-                    Rolls-Royce was a British luxury car and later an aero-engine manufacturing business established in 1904 in Manchester, United Kingdom by the partnership of Charles Rolls and Henry Royce.
-
-                    </p>
-                </div>
-            </div>
+export default function About() {
+  return (
+    <div className="About">
+      <div className="About-container">
+        <div className="About-header">
+          <h1>About</h1>
+          <h1>D21CS110 Swet Soni</h1>
+          <p>
+            Rolls-Royce was a British luxury car and later an aero-engine
+            manufacturing business established in 1904 in Manchester, United
+            Kingdom by the partnership of Charles Rolls and Henry Royce.
+          </p>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
